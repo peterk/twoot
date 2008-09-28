c48135f8fa5013e5121781017e26a1f1f51ac1f4
Peter Krantz <peter.krantz@gmail.com>
2008-09-28 15:12:49 +0200

    Fixed reload issue where last posted message was repeated due to wrong cutoff time.

--- twoot.js
+++ twoot.js
@@ -19,7 +19,9 @@ jQuery.fn.reverse = function() {
 		 
 		 $.getJSON(url, function(data){
 			 $.each(data.reverse(), function(i, item) { 
-				 list.prepend('<li><span class="time">' + relative_time(item.created_at) + '</span> <span class="user" onclick="addAddress(\'' + item.user.screen_name + '\')">' + item.user.screen_name + '</span><div class="tweet_text">' + item.text.replace(/(\w+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+)/, '<a href="$1">$1</a>').replace(/[\@]+([A-Za-z0-9-_]+)/, '<a href="http://twitter.com/$1">@$1</a>').replace(/[&lt;]+[3]/, "<tt class='heart'>&#x2665;</tt>") + '</div></li>');
+				if($("#msg-" + item.id).length == 0) { // <- fix for twitter caching which sometimes have problems with the "since" parameter
+				 	list.prepend('<li id="msg-' + item.id + '"><span class="time">' + relative_time(item.created_at) + '</span> <span class="user" onclick="addAddress(\'' + item.user.screen_name + '\')">' + item.user.screen_name + '</span><div class="tweet_text">' + item.text.replace(/(\w+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+)/, '<a href="$1">$1</a>').replace(/[\@]+([A-Za-z0-9-_]+)/, '<a href="http://twitter.com/$1">@$1</a>').replace(/[&lt;]+[3]/, "<tt class='heart'>&#x2665;</tt>") + '</div></li>');
+				 }
 				 });
 			 });
 		 });
