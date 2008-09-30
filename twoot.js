d41ae325747e659b3bdc0e3464cb76060744ec06
Peter Krantz <peter.krantz@gmail.com>
2008-09-30 22:48:30 +0200

    Automatic recalculation of timestamps each minute.

--- twoot.js
+++ twoot.js
@@ -20,36 +20,48 @@ jQuery.fn.reverse = function() {
 		 $.getJSON(url, function(data){
 			 $.each(data.reverse(), function(i, item) { 
 				if($("#msg-" + item.id).length == 0) { // <- fix for twitter caching which sometimes have problems with the "since" parameter
-				 	list.prepend('<li id="msg-' + item.id + '"><span class="time">' + relative_time(item.created_at) + '</span> <span class="user" onclick="addAddress(\'' + item.user.screen_name + '\')">' + item.user.screen_name + '</span><div class="tweet_text">' + item.text.replace(/(\w+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+)/, '<a href="$1">$1</a>').replace(/[\@]+([A-Za-z0-9-_]+)/, '<a href="http://twitter.com/$1">@$1</a>').replace(/[&lt;]+[3]/, "<tt class='heart'>&#x2665;</tt>") + '</div></li>');
+				 	list.prepend('<li id="msg-' + item.id + '"><span class="time" title="' + item.created_at + '">' + relative_time(item.created_at) + '</span> <span class="user" onclick="addAddress(\'' + item.user.screen_name + '\')">' + item.user.screen_name + '</span><div class="tweet_text">' + item.text.replace(/(\w+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+)/, '<a href="$1">$1</a>').replace(/[\@]+([A-Za-z0-9-_]+)/, '<a href="http://twitter.com/$1">@$1</a>').replace(/[&lt;]+[3]/, "<tt class='heart'>&#x2665;</tt>") + '</div></li>');
 				 }
 				 });
 			 });
 		 });
- function relative_time(time_value) {
-	 var values = time_value.split(" ");
-	 time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
-	 var parsed_date = Date.parse(time_value);
-	 var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
-	 var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
-	 delta = delta + (relative_to.getTimezoneOffset() * 60);
-	 if (delta < 60) {
-		 return '< a minute ago';
-	 } else if(delta < 120) {
-		 return 'a minute ago';
-	 } else if(delta < (45*60)) {
-		 return (parseInt(delta / 60)).toString() + ' minutes ago';
-	 } else if(delta < (90*60)) {
-		 return 'an hour ago';
-	 } else if(delta < (24*60*60)) {
-		 return '' + (parseInt(delta / 3600)).toString() + ' hours ago';
-	 } else if(delta < (48*60*60)) {
-		 return '1 day ago';
-	 } else {
-		 return (parseInt(delta / 86400)).toString() + ' days ago';
-	 }
- };
  };
 })(jQuery);
+
+
+function relative_time(time_value) {
+	var values = time_value.split(" ");
+	time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
+	var parsed_date = Date.parse(time_value);
+	var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
+	var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
+	delta = delta + (relative_to.getTimezoneOffset() * 60);
+	if (delta < 60) {
+		return '< a minute ago';
+	} else if(delta < 120) {
+		return 'a minute ago';
+	} else if(delta < (45*60)) {
+		return (parseInt(delta / 60)).toString() + ' minutes ago';
+	} else if(delta < (90*60)) {
+		return 'an hour ago';
+	} else if(delta < (24*60*60)) {
+		return '' + (parseInt(delta / 3600)).toString() + ' hours ago';
+	} else if(delta < (48*60*60)) {
+		return '1 day ago';
+	} else {
+		return (parseInt(delta / 86400)).toString() + ' days ago';
+	}
+};
+
+
+//get all span.time and recalc from title attribute
+function recalcTime() {
+	$('span.time').each( 
+			function() {
+				$(this).text(relative_time($(this).attr("title")));
+			}
+	)
+}
 
 
 function getSinceParameter() {
@@ -66,6 +78,7 @@ function refreshMessages() {
 	$(".tweets").gettweets();
 
 	LAST_UPDATE = new Date().toGMTString();	
+
 	return;
 }
 
@@ -103,4 +116,7 @@ $(document).ready(function(){
 
 		//set timer to reload messages every 70 secs
 		window.setInterval("refreshMessages()", 70000);
+
+		//set timer to recalc timestamps every 60 secs
+		window.setInterval("recalcTime()", 60000);
 });
